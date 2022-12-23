caed734a6064a0c1c67ca630dba46bd726edbaba
Tyler Howe <tyler.d.howe@gmail.com>
2022-12-23 14:52:45 -0800

    Beta version (2nd place after ThomasBot)

--- AIs/BestBot.py
+++ AIs/BestBot.py
@@ -40,15 +40,17 @@ class CardCounter:
 
 class RowInfo:
     """Helper class to characterize a row"""
-    def __init__(self, row: list[int]):
+    def __init__(self, row: list[int], counter: CardCounter):
         self.value = max(row)
         self.points = Game.getTotalPoints(row)
         self.count = len(row)
-        self.remaining = Game.ROW_SIZE - self.count
+        self.slots_left = Game.ROW_SIZE - self.count
         self.max_value = Game.NUM_CARDS
+        self.possible_count = counter.getCardsInRange(self.value, self.max_value)
 
-    def setNextRow(self, other):
+    def setNextRow(self, other, counter: CardCounter):
         self.max_value = other.value - 1
+        self.possible_count = counter.getCardsInRange(self.value, self.max_value)
 
 class BestBotState:
     def __init__(self, playerCount: int):
@@ -61,77 +63,94 @@ class BestBotState:
         self.round_started = False
         self.card_counter.reset()
 
-    def checkRoundStart(self, hand: list[int], rows: list[list[int]]):
-        if not self.round_started:
+    def prepareTurn(self, hand: list[int], rows: list[list[int]]):
+        if self.round_started:
+            self.cards_remaining = len(hand)
+        else:
             # Start the round
             self.countCards(hand)
             for r in rows:
                 self.countCards(r)
-            self.round_started = True   
+            self.cards_remaining = Game.HAND_SIZE
+            self.round_started = True
+
 
     def countCards(self, cards: list[int]):
         for c in cards:
             self.card_counter.countCard(c)
 
-    def chooseCard(self, hand: list[int], rows: list[list[int]], scores: list[tuple[str, int]]):
-        row_infos = sorted([RowInfo(r) for r in rows], key=lambda r: r.value)
+    def playTurn(self, hand: list[int], rows: list[list[int]], scores: list[tuple[str, int]]):
+        """PLay the turn and select the best card"""
+
+        row_infos = sorted([RowInfo(r, self.card_counter) for r in rows], key=lambda r: r.value)
 
         for i in range(len(row_infos)-1):
             row = row_infos[i]
             next_row = row_infos[i + 1]
-            row.setNextRow(next_row)
+            row.setNextRow(next_row, self.card_counter)
         
-        card_weights = [self._weighCard(c, rows) for c in hand]
+        # Cards are in ascending order
+        weights = [self._weighCard(c, row_infos) for c in hand]
+        min_weight = min(weights)
+        min_cards = []
 
-    def _weighCard(self, card: int, rows: list[list[int]]):
-        """Get the weight for the given card. Range from 0.0 (bad) to 1.0 (good)."""
-        row_maxs = [max(r for r in rows)]
+        for i in range(len(weights)):
+            if weights[i] == min_weight:
+                min_cards.append(hand[i])
+
+        if len(min_cards) > 1:
+            # Multiple cards with same weight.
+            # For now, choose the highest one
+            # TODO: better tiebreaker here.
+            # Ideas: hardest card to play, cards closest to other cards, etc
+            return min_cards[-1]
+        else:
+            # Return only card
+            return min_cards[0]
+
+
+    def _weighCard(self, card: int, rows: list[RowInfo]) -> float:
+        """Get the expected points for the given card. Lower is better"""
 
         # Find the row that this card would play to
-        played_row = -1
-        for i in range(Game.NUM_ROWS):
-            row_max = row_maxs[i]
+        played_row = None
+        for r in rows:
+            if (card > r.value) and (card <= r.max_value):
+                played_row = r
 
-            if row_max < card:
-                played_row = i
-
-        if played_row >= 0:
-            # This card would resolve to at least one row.
+        # TODO: calculate the chance that this row is taken before
+        # it resolves to our card
+        if played_row is not None:
+            # This card would likely resolve to this row.
             # Calculate how likely we are to get points here.
-            return self._weighCardForRow(card, rows[played_row], row_maxs)
+            return self._weighCardForRow(card, played_row)
         else:
             # Card would not resolve to any row.
             return self._weighCardForBreak(card, rows)
 
-    def _weighCardForRow(self, card: int, row: list[int], other_rows: list[int]):
-        """Give a "weight" of playing this card to this row"""
-        # row_max = max(row)
-        # row_cnt = len(row)
-        # slots_left = 5 - row_cnt
+    def _weighCardForRow(self, card: int, row: RowInfo):
+        """Give the expected points of playing this card to this row"""
+        if row.slots_left >= self.player_count:
+            # No chance of it breaking (unless there is fuckery).
+            break_chance = 0.0
+        else:
+            # How likely is this row to break?
 
-        # next_max = 0  ## TODO
+            # How many cards are between this row and our card?
+            remaining = self.card_counter.getCardsInRange(row.value, card - 1)
 
-        # if slots_left <= self.player_count:
-        #     # No chance of it breaking (unless there is fuckery)
-        #     break_chance = 0.0
-        # else:
-        #     # How likely is this row to break?
-        #     next_max = Game.NUM_CARDS if max(other_rows)
-        #     cards_remaining = self.card_counter.getCardsInRange()
-        #     break_chance = slots_left / self.player_count
-        pass
+            # Normalize the weight by number of remaining cards in the round
+            norm = self.cards_remaining / Game.HAND_SIZE
+            break_chance = remaining * norm / self.player_count
+        
+        return break_chance * row.points
 
-    def _weighCardForBreak(self, card: int, rows: list[list[int]]):
-        """Give a "weight" of this card for breaking (taking a row)"""
-
-        # # For now, we naively assume we will just take the min row.
-        # # TODO: improve this here by deciding how difficult this card
-        # # is to play (lower cards are harder to play)
-        # row_max = max(row)
-        # row_cnt = len(row)
-        # slots_left = 5 - row_cnt
-        pass
-
+    def _weighCardForBreak(self, card: int, rows: list[RowInfo]):
+        """Give the expected points of this card for breaking (taking a row)"""
+        # For now, we naively assume we will just take the min row.
+        # TODO: improve this here by deciding how difficult this card
+        # is to play (lower cards are harder to play)
+        return min([r.points for r in rows])
 
 # Setup()
 #   Used to initialize an AI state required later.
@@ -167,8 +186,8 @@ def PostTurn(ai: BestBotState, playedCards: list[int], scores: list[tuple[str, i
 #   A list of the players scores in player order formatted as a tuple (name, score) starting with the current player
 # Returns a card in the player's hand that they intend to play
 def PlayCard(ai: BestBotState, hand: list[int], rows: list[list[int]], scores: list[tuple[str, int]]):
-    ai.checkRoundStart(hand, rows)
-    return ai.chooseCard(hand, rows, scores)
+    ai.prepareTurn(hand, rows)
+    return ai.playTurn(hand, rows, scores)
 
 # ChooseRow()
 #   If the player plays a card lower than the lowest of the cards on row ends, this function is called to choose which row
