ecfe0836ba642d3e9743288c6e846f895395eef6
Tyler Howe <tyler.d.howe@gmail.com>
2022-12-23 13:23:07 -0800

    Round start logic

--- AIs/BestBot.py
+++ AIs/BestBot.py
@@ -22,29 +22,46 @@ class CardCounter:
 
     def countCard(self, card: int):
         """Count the given card"""
-        self.cards_remaining.remove(card)
-        bkt = CardCounter._getBucket(card)
-        self.buckets[bkt] -= 1
+
+        try:
+            self.cards_remaining.remove(card)
+            bkt = CardCounter._getBucket(card)
+            self.buckets[bkt] -= 1
+        except ValueError:
+            pass # Already counted
 
     @staticmethod
     def _getBucket(card: int):
         return round(card * CardCounter.NUM_BUCKETS / Game.NUM_CARDS)
 
 class BestBotState:
-    def __init__(self, playerCount):
+    def __init__(self, playerCount: int):
         self.card_counter = CardCounter(playerCount)
         self.reset()
         
     def reset(self):
         """Reset to inital state"""
+        self.round_started = False
         self.card_counter.reset()
+
+    def checkRoundStart(self, hand: list[int], rows: list[list[int]]):
+        if not self.round_started:
+            # Start the round
+            self.countCards(hand)
+            for r in rows:
+                self.countCards(r)
+            self.round_started = True   
+
+    def countCards(self, cards: list[int]):
+        for c in cards:
+            self.card_counter.countCard(c)
 
 # Setup()
 #   Used to initialize an AI state required later.
 # Takes arguments:
 #   Number of players
 # Returns optionally an object containing the AI's state
-def Setup(playerCount):
+def Setup(playerCount: int):
     return BestBotState(playerCount)
 
 # PostRound()
@@ -52,7 +69,7 @@ def Setup(playerCount):
 # Takes arguments:
 #   The player AI object, or None if no such object was created
 #   A list of the players scores in player order formatted as a tuple (name, score) starting with the current player
-def PostRound(ai: BestBotState, scores):
+def PostRound(ai: BestBotState, scores: list[tuple[str, int]]):
     ai.reset()
             
 # PostTurn()
@@ -61,9 +78,8 @@ def PostRound(ai: BestBotState, scores):
 #   The player AI object, or None if no such object was created
 #   A list of the players cards in player order starting with the current player
 #   A list of the players scores in player order formatted as a tuple (name, score) starting with the current player
-def PostTurn(ai: BestBotState, playedCards, scores):
-    for card in playedCards:
-        ai.card_counter.countCard(card)
+def PostTurn(ai: BestBotState, playedCards: list[int], scores: list[tuple[str, int]]):
+    ai.countCards(playedCards)
 
 # PlayCard()
 #   Determines which card from the player's hand they should play
@@ -73,7 +89,11 @@ def PostTurn(ai: BestBotState, playedCards, scores):
 #   A list of lists representing the current state of the card rows
 #   A list of the players scores in player order formatted as a tuple (name, score) starting with the current player
 # Returns a card in the player's hand that they intend to play
-def PlayCard(ai: BestBotState, hand: list[int], rows: list[list[int]], scores):
+def PlayCard(ai: BestBotState, hand: list[int], rows: list[list[int]], scores: list[tuple[str, int]]):
+    ai.checkRoundStart(hand, rows)
+    
+    
+
     return max(hand)
 
 # ChooseRow()
@@ -86,7 +106,7 @@ def PlayCard(ai: BestBotState, hand: list[int], rows: list[list[int]], scores):
 #   A list of all of the card played this turn
 #   A list of the players scores in player order formatted as a tuple (name, score) starting with the current player
 # Returns the index of the row the player has chosen
-def ChooseRow(ai, card, hand, rows, cardPlayed, scores):
+def ChooseRow(ai, card: int, hand: list[int], rows: list[list[int]], cardsPlayed: list[int], scores: list[tuple[str, int]]):
     rowScores = [Game.getTotalPoints(r) for r in rows]
     minRow = rowScores.index(min(rowScores))
     return minRow
